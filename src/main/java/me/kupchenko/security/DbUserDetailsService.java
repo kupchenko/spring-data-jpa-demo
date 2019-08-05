450de373dd85a6dfe85302c4ba4b53c7326ba2c3
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2019-08-05 21:01:28 +0300

    Refactor code

--- src/main/java/me/kupchenko/security/DbUserDetailsService.java
+++ src/main/java/me/kupchenko/security/DbUserDetailsService.java
@@ -5,14 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import me.kupchenko.dao.UserRepository;
 import me.kupchenko.model.Role;
 import me.kupchenko.model.User;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -23,8 +20,7 @@ public class DbUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("Loading '{}' info... ", username);
-        Optional<User> userOptional = userRepository.findByName(username);
-        User user = userOptional.orElseThrow(() -> {
+        User user = userRepository.findByName(username).orElseThrow(() -> {
             log.warn("User not found with username: {}", username);
             return new UsernameNotFoundException("No user found for username: " + username);
         });
